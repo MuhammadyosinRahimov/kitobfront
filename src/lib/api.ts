a946fc4f45e88895a40ad750e767fa9f93e8c7a7
DOC <nushajon@gmail.com>
2026-01-20 15:14:44 +0500

    bug with env

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3004';
+const API_URL = (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3004').replace(/\/$/, '');
 
 export const api = axios.create({
   baseURL: API_URL,
